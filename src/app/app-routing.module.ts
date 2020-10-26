46977148cd51de33a58a92b386988f614b57a744
Rahul Sharma <rahu8299@gmail.com>
2020-10-26 15:00:37 +0530

    removing contect me page + some bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ContectMeComponent } from './contect-me/contect-me.component';
 import { JsprojecgtsComponent } from './jsprojecgts/jsprojecgts.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
@@ -14,12 +13,6 @@ const routes: Routes = [{
   path: "js-projects",
   component: JsprojecgtsComponent,
 },
-
-{
-  path: "contact-me",
-  component: ContectMeComponent,
-},
-
 {
   path: "**",
   component: NotFoundComponent,
